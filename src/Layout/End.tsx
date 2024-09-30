edabc95e218902852df06fa7e86a9bd18e8fa174
Chi Kit <kitchi734@gmail.com>
2024-09-30 14:53:33 -0700

    text stork

--- src/Layout/End.tsx
+++ src/Layout/End.tsx
@@ -4,15 +4,18 @@ import Footer from "./Footer";
 import Text from "../assets/img/text.png";
 
 const End: React.FC = () => {
-
   
+
   return (
     <section className="relative h-full" id="howtobuy">
       <div className="absolute top-5 justify-center flex w-full">
         <img src={Text} alt="" className="lg:w-[600px] md:w-[300px] w-[250px] mx-auto" />
       </div>
       <img src={bg} alt="background" className="w-full h-[50vw]" />
-      <Footer />
+      {
+        
+        <Footer />
+      }
     </section>
   );
 };
--- src/Layout/Footer.tsx
+++ src/Layout/Footer.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import dex from "../assets/img/threreallandwolf.svg";
 import telegram from "../assets/img/telegram.svg";
 import x from "../assets/img/x.svg";
 import UpArrow from "../assets/img/arrow-up.svg";
 
 const Footer: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    // Check if the user has scrolled to the bottom of the page
+    const isAtBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight;
+    setIsVisible(isAtBottom);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
   return (
-    <nav className="navigation">
+    <nav className="navigation fixed bottom-0 left-0 z-[1000] !opacity-100">
       <ul className="navigation_ul">
         <li>
           <a
@@ -39,14 +54,24 @@ const Footer: React.FC = () => {
           </a>
         </li>
       </ul>
-      <div className="footer_disclaimer">
-        <p className="text-white">Copyright © Sparky || All Rights Reserved.</p>
-      </div>
-      <div className="to_top">
-        <a id="scroll-top" href="#home" className="bg-white rounded-full p-1" >
-          <img src={UpArrow} alt="up" />
-        </a>
-      </div>
+      {isVisible && 
+        <>
+          <div className="footer_disclaimer">
+            <p className="text-white">
+              Copyright © Sparky || All Rights Reserved.
+            </p>
+          </div>
+          <div className="to_top">
+            <a
+              id="scroll-top"
+              href="#home"
+              className="bg-white rounded-full p-1"
+            >
+              <img src={UpArrow} alt="up" />
+            </a>
+          </div>
+        </>
+      }
     </nav>
   );
 };
