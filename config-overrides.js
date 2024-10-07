92f3d322758b9c9395a016e8988d73a61930ee2d
Chi Kit <kitchi734@gmail.com>
2024-10-07 05:02:52 -0700

    all refresh

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-module.exports = function override(config, env) {
-  config.ignoreWarnings = [
-    {
-      module: /source-map-loader/,
-      message: /Failed to parse source map/,
-    },
-  ];
-  return config;
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                "Marker": "Marker"
-            },
-            backgroundColor: {
-                "gradient": "linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgb(210 49 249) 0%, rgb(44 237 170) 100%)"
-            },
-            textShadow: {
-                'custom': '-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black',
-                'sm': '-4px 0 black, 0 4px black, 4px 0 black, 0 -4px black',
-                'md': '-6px 0 black, 0 6px black, 6px 0 black, 0 -6px black',
-                'lg': '-8px 0 black, 0 8px black, 8px 0 black, 0 -8px black',
-            }
-        },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
     },
-    plugins: [
-        require('tailwindcss-textshadow'), // Add the text shadow plugin
-    ],
-}
+  },
+  plugins: [],
+};
